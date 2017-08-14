d09c9dcefdb50af1e9c95f46ce3324a1fa777a5e
fliem <franz.liem@gmail.com>
2017-08-14 14:24:05 +0200

    changes update [skip ci]

--- baracus/__init__.py
+++ baracus/__init__.py
@@ -3,9 +3,10 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir_path, "../version")) as fi:
     __version__ = fi.read().strip()
 
-models_list =  ["Liem2016__OCI_norm", "Liem2016__full_2samp_training"]
+models_list = ["Liem2016__OCI_norm", "Liem2016__full_2samp_training"]
 
 __changes__ = """
+* 1.0.0: compute recon-all
 * 0.9.4: fixed models in brain_age_files
 * 0.9.3: fix tagging issue
 * 0.9.2: fixed typo, -v flag
